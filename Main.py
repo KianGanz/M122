264fecf7d65d01d838239f8d14f11d13e4d818c0
Kian Ganz <kian.ganz@gmx.ch>
2022-06-28 16:04:56 +0200

    dotenv

--- Main.py
+++ Main.py
@@ -1,5 +1,4 @@
 # See your keys here: https://dashboard.stripe.com/apikeys
-stripe.api_key = 'sk_test_51LFeLQIPHMm43ITmL8NdxFkRDd2S82yh98lHe9lCJTRf8BPRsN5sa9LRkqItnawVkkATQAwOHSZb2p3pd5PrFa3F00aDg08c6b'
 
 stripe.PaymentIntent.create(
   amount=1099,
