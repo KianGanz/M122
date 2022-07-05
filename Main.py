953cc2e39f7d043259ece96af31fc201c572a501
Kian Ganz <kian.ganz@gmx.ch>
2022-07-05 13:30:19 +0200

    stripe-API_Key

--- Main.py
+++ Main.py
@@ -1,4 +1,13 @@
 # See your keys here: https://dashboard.stripe.com/apikeys
+import stripe
+import os
+
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+print(os.getenv('stripe.api_key'))
+
 
 stripe.PaymentIntent.create(
   amount=1099,
