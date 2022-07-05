dc19438cd301084818891d31eed62c52926a569b
Kian Ganz <kian.ganz@gmx.ch>
2022-07-05 14:20:05 +0200

    testing dotenv

--- Main.py
+++ Main.py
@@ -6,11 +6,12 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-print(os.getenv('stripe.api_key'))
+print(os.getenv('stripe_secret'))
 
-
+"""
 stripe.PaymentIntent.create(
   amount=1099,
   currency='chf',
   payment_method_types=['card'],
 )
+"""
