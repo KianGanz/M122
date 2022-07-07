0ef4940e238ec11ee788140f14c2fd0a06dc41e3
Kian Ganz <kian.ganz@gmx.ch>
2022-07-08 00:06:39 +0200

    pdf

--- Main.py
+++ Main.py
@@ -1,17 +1,54 @@
 # See your keys here: https://dashboard.stripe.com/apikeys
 import stripe
 import os
+from dotenv import load_dotenv
+from reportlab.pdfgen import canvas
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase import pdfmetrics
+from reportlab.lib import colors
 
-from dotenv import load_dotenv, find_dotenv
+load_dotenv()
 
-load_dotenv(find_dotenv())
+api_key = os.getenv('stripe.api_key')
 
-print(os.getenv('stripe_secret'))
+stripe.api_key = api_key
 
-"""
-stripe.PaymentIntent.create(
-  amount=1099,
-  currency='chf',
-  payment_method_types=['card'],
-)
-"""
+charges = stripe.Charge.list(limit=1)
+
+test = print(charges)
+
+fileName = 'Betrag.pdf'
+documentTitle = 'sample'
+title = 'Modul 122'
+subTitle = 'Betrag Informationen'
+textLines = print(charges)
+image = 'OIP.jfif'
+
+pdf = canvas.Canvas(fileName)
+
+pdf.setTitle(documentTitle)
+
+pdf.drawCentredString(300, 770, title)
+
+pdf.setFillColorRGB(0, 0, 255)
+pdf.setFont("Courier-Bold", 24)
+pdf.drawCentredString(290, 720, subTitle)
+
+pdf.line(30, 710, 550, 710)
+
+text = pdf.beginText(40, 680)
+text.setFont("Courier", 18)
+text.setFillColor(colors.red)
+  
+for line in textLines:
+    text.textLine(line)
+      
+pdf.drawText(text)
+
+pdf.drawInlineImage(image, 130, 400)
+
+pdf.save()
+
+
+
+
